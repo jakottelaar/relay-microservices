2d9b0a276d38e57eeb45751c3427f75be87581d2
jakottelaar <jarno.kottelaar@gmail.com>
2026-06-04 22:48:55 +0200

    Updated hub.go to handle multiple clients / connections for the same user

--- services/gateway/internal/hub.go
+++ services/gateway/internal/hub.go
@@ -3,7 +3,7 @@ package internal
 import "go.uber.org/zap"
 
 type Hub struct {
-	clients map[int64]*Client
+	clients map[int64]map[*Client]struct{}
 
 	register   chan *Client
 	unregister chan *Client
@@ -14,7 +14,7 @@ type Hub struct {
 
 func NewHub(log *zap.Logger) *Hub {
 	return &Hub{
-		clients: make(map[int64]*Client),
+		clients: make(map[int64]map[*Client]struct{}),
 		register: make(chan *Client),
 		unregister: make(chan *Client),
 		broadcast: make(chan *Event),
@@ -26,18 +26,26 @@ func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
-			h.clients[client.userID] = client
+			if _, ok := h.clients[client.userID]; !ok {
+				h.clients[client.userID] = make(map[*Client]struct{})
+			}
+			h.clients[client.userID][client] = struct{}{}
 			h.log.Info("Client registered", 
 				zap.Int64("userID", client.userID),
 			)
 
 		case client := <-h.unregister:
-			if _, ok := h.clients[client.userID]; ok {
-				delete(h.clients, client.userID)
-				close(client.send)
-				h.log.Info("client disconnected",
-					zap.Int64("userID", client.userID),
-				)
+			if clients, ok := h.clients[client.userID]; ok {
+				if _, ok := clients[client]; ok {
+					delete(clients, client)
+					close(client.send)
+					if len(clients) == 0 {
+						delete(h.clients, client.userID)
+					}
+					h.log.Info("client disconnected",
+						zap.Int64("userID", client.userID),
+					)
+				}
 			}
 		case event := <-h.broadcast:
 			h.dispatch(event)
@@ -55,19 +63,36 @@ func (h *Hub) dispatch(event *Event) {
 		return
 	}
 
+	h.log.Info("dispatching websocket event",
+		zap.String("event", event.Type),
+		zap.Int("recipient_count", len(event.Recipients)),
+	)
+
 	for _, userID := range event.Recipients {
-		client, ok := h.clients[userID]
+		clients, ok := h.clients[userID]
 		if !ok {
+			h.log.Debug("no connected client for recipient",
+				zap.Int64("userID", userID),
+			)
 			continue
 		}
-		select {
-		case client.send <- data:
-		default:
-			h.log.Warn("client buffer full, disconnecting",
-			zap.Int64("userID", userID))
-			
-			delete(h.clients, userID)
-			close(client.send)
+		for client := range clients {
+			select {
+			case client.send <- data:
+				h.log.Debug("websocket event queued",
+					zap.Int64("userID", userID),
+				)
+			default:
+				h.log.Warn("client buffer full, disconnecting",
+					zap.Int64("userID", userID),
+				)
+
+				delete(clients, client)
+				close(client.send)
+				if len(clients) == 0 {
+					delete(h.clients, userID)
+				}
+			}
 		}
 	}
 }
